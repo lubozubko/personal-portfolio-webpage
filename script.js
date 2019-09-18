ffb129d0a9c078cec4ec025502bd55d4c8eab01b
Lubomir Zubko <32295327+lubozubko@users.noreply.github.com>
2019-09-18 07:41:16 +0100

    deleted script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// navbar menu button
-function classToggle() {
-    const navs = document.querySelectorAll('.nav-list')
-
-    navs.forEach(nav => nav.classList.toggle('nav-toggle-show'));
-}
-
-document.querySelector('.nav-links-toggle')
-    .addEventListener('click', classToggle);
